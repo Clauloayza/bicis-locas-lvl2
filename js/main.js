8df257c472bae1cb04f09462f5e07dc0554ef4dc
claudia <clou.7loymed@gmail.com>
2017-02-22 14:50:00 -0500

    jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 function cuadroSpan(input,message){
 	
 	if(input.find('span').length == 0){
@@ -26,10 +27,7 @@ function validateM(){
 //GLOBAL
 $(document).ready(function(){
 	$('#name').keyup(validateM);
-	$('#lastname').keyup(validateA);
-	$('#input-email').keyup(validateEmail);
-	$('#input-password').keyup(validatePassword);
-	$('#select').keyup(validateSelect);
+	
 })
 
 function validateForm(){
